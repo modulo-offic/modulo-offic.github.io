30d6eba326e5b098a54a1a4210506dae5fe778cf
DGDays(oleg) <oleg.pariev04@mail.ru>
2020-10-27 16:25:49 +0500

    Куки!

--- assets/js/toogle.js
+++ assets/js/toogle.js
@@ -1,3 +1,34 @@
+const switchTumblerHandler = () => {
+const wrapper = document.querySelector('.tumbler__wrapper');
+    wrapper.addEventListener('click', () => {
+    toggleNightMode();
+    });
+};                    
+
+const toggleNightMode = () => {
+document.body.classList.toggle('body--night-mode');
+document.querySelector('.tumbler').classList.toggle('tumbler--night-mode');
+document.querySelectorAll('.post').forEach(post => {
+    post.classList.toggle('post--night-mode');
+    let theme_click = getCookie("theme");
+    if (theme_click == "lite"){
+        let date = new Date(Date.now());
+        date.setMonth(date.getMonth + 12);
+        date = date.toUTCString();
+        document.cookie = "theme=dark; path=/; expires="+date;
+        console.log(document.cookie);
+    
+    }else{
+        let date = new Date(Date.now());
+        date.setMonth(date.getMonth + 12);
+        date = date.toUTCString();
+        document.cookie = "theme=lite; path=/; expires="+date;
+        console.log(document.cookie);
+}
+});
+};
+
+
 // возвращает куки с указанным name,
 // или undefined, если ничего не найдено
 function getCookie(name) {
@@ -6,18 +37,12 @@ function getCookie(name) {
     ));
     return matches ? decodeURIComponent(matches[1]) : undefined;
 };
-    
+
+
 var theme = getCookie("theme");
-if (theme == "lite"){
-    let date = new Date(Date.now());
-    date.setMonth(date.getMonth + 12);
-    date = date.toUTCString();
-    document.cookie = "theme=dark; path=/; expires="+date;
-    console.log(document.cookie);
-}else{
-    let date = new Date(Date.now());
-    date.setMonth(date.getMonth + 12);
-    date = date.toUTCString();
-    document.cookie = "theme=lite; path=/; expires="+date;
-    console.log(document.cookie);
+if (theme == "dark"){
+    toggleNightMode();
 }
+
+
+switchTumblerHandler();
