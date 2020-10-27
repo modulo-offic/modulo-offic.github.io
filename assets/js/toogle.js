e0d792a393d73be745d3b3d4c3fca78063b0de90
DGDays(oleg) <oleg.pariev04@mail.ru>
2020-10-27 10:43:25 +0500

    Добавил куки снова)

---
+++ assets/js/toogle.js
@@ -0,0 +1,23 @@
+// возвращает куки с указанным name,
+    // или undefined, если ничего не найдено
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+};
+    
+var theme = getCookie("theme");
+if (theme == "lite"){
+    let date = Date.now();
+    date.setMonth(date.getMonth() + 12);
+    date = date.toUTCString();
+    document.cookie = "theme=dark; path=/; expires="+date;
+    console.log(document.cookie);
+}else{
+    let date = Date.now();
+    date.setMonth(date.getMonth() + 12);
+    date = date.toUTCString();
+    document.cookie = "theme=lite; path=/; expires="+date;
+    console.log(document.cookie);
+}
