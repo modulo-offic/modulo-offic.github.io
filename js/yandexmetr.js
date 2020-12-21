b8a3531454599d1a47002a024ce97678aeb40cdc
zakhar24 <53836202+zakhar24@users.noreply.github.com>
2020-12-21 13:01:09 +0500

    оптимизация

--- js/yandexmetr.js
+++ js/yandexmetr.js
@@ -25,3 +25,18 @@
         };
       });
 })();
+
+// отключать :hover-состояния при прокрутке.
+var body = document.body,
+    timer;
+ 
+window.addEventListener('scroll', function() {
+  clearTimeout(timer);
+  if(!body.classList.contains('disable-hover')) {
+    body.classList.add('disable-hover')
+  }
+ 
+  timer = setTimeout(function(){
+    body.classList.remove('disable-hover')
+  }, 500);
+}, false);
