576addf56986169c0c566d0e5493516e7d560155
zakhar24 <53836202+zakhar24@users.noreply.github.com>
2021-01-14 22:25:29 +0500

    optimization

---
+++ js/adv.js
@@ -0,0 +1,16 @@
+(function(w, d, n, s, t) {
+    w[n] = w[n] || [];
+    w[n].push(function() {
+        Ya.Context.AdvManager.render({
+            blockId: "R-A-679020-2",
+            renderTo: "yandex_rtb_R-A-679020-2",
+            async: true
+        });
+    });
+    t = d.getElementsByTagName("script")[0];
+    s = d.createElement("script");
+    s.type = "text/javascript";
+    s.src = "//an.yandex.ru/system/context.js";
+    s.async = true;
+    t.parentNode.insertBefore(s, t);
+})(this, this.document, "yandexContextAsyncCallbacks");
