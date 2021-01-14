49d09c2c91597b71bff91980172b779069317a65
zakhar24 <53836202+zakhar24@users.noreply.github.com>
2021-01-14 22:07:45 +0500

    optimization

--- js/yandexmetr.js
+++ js/yandexmetr.js
@@ -1,5 +1,4 @@
-function onload() {
-  (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
     (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
@@ -26,7 +25,6 @@ function onload() {
         };
       });
 })();
-};
 
 // отключать :hover-состояния при прокрутке.
 var body = document.body,
